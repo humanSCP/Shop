cbab69ff7614b8a975e803d18ec264e96d2e5edf
humanSCP <codethue123@gmail.com>
2023-02-15 16:56:25 +0700

    [15/02/2023] coding banner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../components/banner/Banner'
+import BannerSale from '../components/banner/BannerSale'
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <div className="overflow-hidden">
+            <Banner></Banner>
+            <BannerSale></BannerSale>
+        </div>
+    )
 }
 
 export default HomePage
